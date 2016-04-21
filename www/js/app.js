dded4fbea46e4e5cc34b5b20aabfc0145283127c
hikmahgumelar <hikmahgumelar@gmail.com>
2016-04-21 16:04:23 +0700

    Commit pertama kali - input text asal - input text tujuan - proxy API ke penyedia API

---
+++ www/js/app.js
@@ -0,0 +1,19 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic'])
+ 
+  .controller("Ambildata", function ($scope, $http) {
+  var asal = $scope.asal
+    $scope.proses = function() {  
+
+    $http.get('/api/keretaapi?origination='+asal+'&destination=BD&tanggal=20160423&apikey=TzW9lTM0RBsm2AgnL2M3AZgCZCcwz5hx_1412403086').success(function (data) {
+$scope.events = data;
+$scope.gugum = "data keretaapi";
+     });
+    };
+             
+
+});
